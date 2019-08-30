b0d06debd6be72944560dfcf5dbe43620a9fc2dc
Sebastian Riedel <git@basxto.de>
2019-08-30 13:47:12 +0200

    Build libvorbis for arm-linux-gnueabihf

--- libvorbis.sh
+++ libvorbis.sh
@@ -2,7 +2,7 @@
 
 pushd $PWD/libvorbis-1.3.6
     export CFLAGS="-g"
-    ./configure --prefix=$PWD/install --enable-shared=no --with-ogg=$PWD/../libogg-1.3.3/install
+    ./configure --prefix=$PWD/install --enable-shared=no --with-ogg=$PWD/../libogg-1.3.3/install --with-ogg-includes=$PWD/../libogg-1.3.3/include --host=arm-linux-gnueabihf
     make
     make install
 popd
