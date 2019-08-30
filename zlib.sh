59e46ce104db22486b5d7df8710b102984b686b6
Sebastian Riedel <git@basxto.de>
2019-08-30 14:47:44 +0200

    Build zlib for arm-linux-gnueabihf

--- zlib.sh
+++ zlib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd $PWD/zlib-1.2.11
-    ./configure --static --prefix=$PWD/install
+    CC=arm-linux-gnueabihf-gcc AR=arm-linux-gnueabihf-ar ./configure --static --prefix=$PWD/install
     make
     make install
 popd
