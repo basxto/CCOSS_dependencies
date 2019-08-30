9b9363c3e39816d478a0fe2e78befdad274f761a
Sebastian Riedel <git@basxto.de>
2019-08-30 17:44:49 +0200

    Build minizip for arm-linux-gnueabihf

--- minizip.sh
+++ minizip.sh
@@ -4,7 +4,9 @@ source dirs.sh
 pushd $MINIZIP_DIR
     mkdir build
     cd build
-    cmake -DCMAKE_INSTALL_PREFIX=$PWD/../install ../
+    cmake -DCMAKE_TOOLCHAIN_FILE=$PWD/../../arm-linux-gnueabihf-toolchain.cmake -DCMAKE_INSTALL_PREFIX=$PWD/../install ../
+    # no idea why
+    cp ZLIB_DIR/*.h .
     make
     make install
 popd
