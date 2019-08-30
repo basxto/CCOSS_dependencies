c58f96f66ba01c9f3d2084037fe28a5debc01296
Sebastian Riedel <git@basxto.de>
2019-08-30 14:52:05 +0200

    Build libpng for arm-linux-gnueabihf

--- libpng.sh
+++ libpng.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd $PWD/libpng-1.6.37
-    ./configure --prefix=$PWD/install --enable-shared=no --with-sysroot=$PWD/../zlib-1.2.11/install
+    ./configure --prefix=$PWD/install --enable-shared=no --with-sysroot=$PWD/../zlib-1.2.11/install --host=arm-linux-gnueabihf CPPFLAGS="-I$PWD/../zlib-1.2.11/install/include" LDFLAGS="-L$PWD/../zlib-1.2.11/install/lib"
     make
     make install
 popd
