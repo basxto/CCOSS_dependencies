62d166223da50fbcf1fdb7648414ef52fb4209d4
Sebastian Riedel <git@basxto.de>
2019-08-31 00:11:48 +0200

    Fix libvorbis

--- libvorbis.sh
+++ libvorbis.sh
@@ -2,8 +2,8 @@
 source dirs.sh
 
 pushd $VORBIS_DIR
-    export CFLAGS="-g"
-    ./configure --prefix=$PWD/install --enable-shared=no --with-ogg=$OGG_DIR/install --with-ogg-includes=$OGG_DIR/include --host=arm-linux-gnueabihf
+    export CFLAGS="-g -I$OGG_DIR/include"
+    ./configure --prefix=$PWD/install --enable-shared=no --with-ogg=$OGG_DIR/install --host=arm-linux-gnueabihf
     make
     make install
 popd
